3d21c3bcdfffa76d74a0f10d08b60857313870ba
big-gsu <genielaunch@gmail.com>
2025-12-31 17:13:51 -0600

    Add files via upload

---
+++ dist/glsa-chatui.js
@@ -0,0 +1,13 @@
+import e from"https://cdn.jsdelivr.net/gh/big-gsu/big-chatagent-src@v2025.12.30/dist/pole-embed-yard.js";e.initFull({n8nChatUrl:"https://n8n.srv1194916.hstgr.cloud/webhook/64bfc1a9-76f7-4f57-9cc5-5d9eda32b056/chat",metadata:{},theme:{button:{iconColor:"#ffffff",backgroundColor:"#042b5f"},chatWindow:{borderRadiusStyle:"rounded",avatarBorderRadius:25,messageBorderRadius:18,showTitle:!0,title:"GENIE Startup Advisor",titleAvatarSrc:"https://raw.githubusercontent.com/big-gsu/gfx/main/genie-g1-161x145.png",avatarSize:35,welcomeMessage:`Hello! I\u2019m Al, your startup advisor with the GENIE program. I can help you with starting or growing your business here in Georgia.  My support includes:
+
+\u2022 Guiding you through the business startup process (from idea validation to early customer acquisition).
+
+\u2022 Offering step-by-step instructions and practical templates.
+
+\u2022 Explaining legal, financial, and organizational requirements specific to Georgia.
+
+\u2022 Recommending local and state resources, including support programs and educational opportunities.
+
+\u2022 Helping you understand and access resources through organizations like the Business Innovation Group (BIG) at Georgia Southern University.
+
+To get started, could you share where you are in your startup journey? Or feel free to ask me any specific questions about the challenges you\u2019re facing.`,errorMessage:"Unable to connect. Please try again later.",backgroundColor:"#ffffff",height:0,width:0,fontSize:16,starterPromptFontSize:13,renderHTML:!0,clearChatOnReload:!1,showScrollbar:!0,botMessage:{backgroundColor:"#f4f8ff",textColor:"#050505",showAvatar:!0,avatarSrc:"https://raw.githubusercontent.com/big-gsu/gfx/main/chaticon-g1.png",showCopyToClipboardIcon:!0},userMessage:{backgroundColor:"#3B81F6",textColor:"#fafafa",showAvatar:!1,avatarSrc:"https://www.svgrepo.com/show/532363/user-alt-1.svg"},textInput:{placeholder:"Enter your question or idea here",backgroundColor:"#ffffff",textColor:"#1e1e1f",sendButtonColor:"#042b5f",maxChars:3e3,maxCharsWarningMessage:"You exceeded the characters limit. Please input less than 3000 characters.",autoFocus:!1,borderRadius:12,sendButtonBorderRadius:25},footer:{company:"BIG",text:"Powered by",companyLink:"https://ww2.georgiasouthern.edu/research/big/"},uploadsConfig:{enabled:!0,acceptFileTypes:["png","jpeg","jpg","pdf","txt","md","doc","xls","csv","xlsx","rtf","docx"],maxSizeInMB:5,maxFiles:1}}}});
